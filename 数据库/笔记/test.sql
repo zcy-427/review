faad8c129d3a09b40da845a5717f78b3ede2117d
Amadeus <2593251697@qq.com>
2026-07-02 14:23:12 +0800

    vault backup: 2026-07-02 14:23:12

--- 数据库/笔记/test.sql
+++ 数据库/笔记/test.sql
@@ -1,3 +1,5 @@
 1. SELECT Sno,Sname FROM student
 
-2. SELECT Sno,Sname FROM student WHERE Dept
+2. SELECT Sno,Sname FROM student WHERE Dept='软件工程'
+
+3. SELECT Sno,Cno,Grade FROM SC WHERE Grade>=60
